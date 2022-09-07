669d81eeebc70deaa5347c00aff96d845ec184f0
neerajkashetty <neeraj.kashetty29@gmail.com>
2022-09-07 11:24:12 +0530

    Functions Basics

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,32 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-  const enteredNumber = parseInt(userInput.value);
-  calcDescription = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber
+function FinalOutput(PrevNumber , operator , calcNumber){
+  const calcDescription = `${PrevNumber}  ${operator} ${calcNumber}`;
   outputResult(currentResult, calcDescription);
 }
 
+function add() {
+  const enteredNumber = parseInt(userInput.value);
+  const intialNumber = currentResult
+  currentResult = currentResult + enteredNumber
+  FinalOutput(intialNumber, '+' , enteredNumber)
+}
+function subtract(){
+  const enteredNumber = parseInt(userInput.value);
+  const intialNumber = currentResult
+  currentResult = currentResult - enteredNumber
+  FinalOutput(intialNumber, '-' , enteredNumber)
+}
+
+function multiply(){
+  const enteredNumber = parseInt(userInput.value);
+  const intialNumber = currentResult
+  currentResult = currentResult * enteredNumber
+  FinalOutput(intialNumber, 'x' , enteredNumber)
+
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
